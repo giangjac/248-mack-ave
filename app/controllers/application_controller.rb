189812b7aaee03f0f4b864e3088ac2ebe4576b42
Jacky Giang <giangjac@gmail.com>
2019-09-02 18:47:22 +0000

    Add exclamation point for added emphasis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def hello
-    render html: "Placeholder for the HOA of 248 Mack Ave"
+    render html: "Placeholder for the HOA of 248 Mack Ave!"
   end
 end
